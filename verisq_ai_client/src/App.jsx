34fd4a7900b01446fc41a0e0cdcf6bc52e61a606
vigneshchitroda <vigneshchitroda@gmail.com>
2026-04-06 17:02:16 +0530

    Vendor Model Added with previous Push

--- verisq_ai_client/src/App.jsx
+++ verisq_ai_client/src/App.jsx
@@ -4,8 +4,13 @@ import VerifyPage from './pages/VerifyPage'
 import ProtectedRoute from './routes/ProtectedRoute'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import RequestReceived from './pages/RequestReceived'
+
 import Otp_verification from './pages/Otp_Verification'
 import AdminTrialRequests from './pages/AdminTrialRequests'
+
+import Howitworks from './pages/Howitworks'
+
+
 
 
 function App() {
@@ -14,6 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/verify" element={<VerifyPage />} />
+
         <Route path="/request-received" element={<RequestReceived />} />
         <Route path="/send-code" element={<Otp_verification />} />
         <Route path="/admin/trial-requests" element={<AdminTrialRequests />} />
@@ -24,6 +30,17 @@ function App() {
               <DashboardPage />
             </ProtectedRoute>
           } />
+        <Route path="/otp_verification" element={<Otp_verification/>}/>
+       <Route path="/request-received" element={<RequestReceived />} />
+       <Route path="how-it-works" element={<Howitworks/>}/>
+        <Route 
+        path="/dashboard" 
+        element={
+          <ProtectedRoute>
+            <DashboardPage/>
+          </ProtectedRoute>
+        }/>
+
       </Routes>
     </BrowserRouter>
   )
