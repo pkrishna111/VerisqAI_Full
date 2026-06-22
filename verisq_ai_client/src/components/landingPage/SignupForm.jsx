a357605d98f2a8adabe786322c3d9949f5db04cf
pkrishna111 <krishna.lidolearning.02@gmail.com>
2026-06-22 16:09:40 +0530

    Gmail public domain disabled and Deploy VerisqAI to Supabase, Render and Vercel with Resend email integration

--- verisq_ai_client/src/components/landingPage/SignupForm.jsx
+++ verisq_ai_client/src/components/landingPage/SignupForm.jsx
@@ -30,7 +30,7 @@ function SignupForm() {
 
     const validateField = (name, value, currentFormData) => {
         const publicDomains = [
-            // "gmail.com",
+            "gmail.com",
             "yahoo.com",
             "hotmail.com",
             "outlook.com",
@@ -148,7 +148,7 @@ function SignupForm() {
         setMessage("");
 
         const publicDomains = [
-            // "gmail.com",
+            "gmail.com",
             "yahoo.com",
             "hotmail.com",
             "outlook.com",
